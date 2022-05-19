02fe119db8ac9b9dfec8c137e081140c740dcefe
mfrasl <104164997+mfrasl@users.noreply.github.com>
2022-05-18 20:03:08 -0500

    Adding initial files.
    Practice with data types.

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args) {
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float minimum value = " + myMinFloatValue);
+        System.out.println("Float maximum value = " + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double minimum value = " + myMinDoubleValue);
+        System.out.println("Double maximum value = " + myMaxDoubleValue);
+
+        int myIntValue = 5;
+        float myFloatValue = 5F;
+        double myDoubleValue = 5D;
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("myFloatValue = " + myFloatValue);
+        System.out.println("myDoubleValue = " + myDoubleValue);
+
+        //pounds to kilograms challenge
+        double numberOfPounds = 445D;
+        double convertedKiloGrams = 0.45359237 * numberOfPounds;
+        System.out.println("Converted Kilograms = " + convertedKiloGrams);
+    }
+}
